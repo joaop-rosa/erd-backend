6afb35016be5c5135f1b8d2b78c8380cc68e341d
Xonho <jp.ragdool@hotmail.com>
2023-11-23 21:23:05 -0300

    a

--- routes/effectTypeRoutes.js
+++ routes/effectTypeRoutes.js
@@ -12,4 +12,15 @@ effectTypeRoutes.get('/', async (req, res) => {
   }
 })
 
+effectTypeRoutes.post('/', async (req, res) => {
+  console.log('req', req.body)
+  if (req.body.name) {
+    res.status(200).json({ sucess: true })
+  } else {
+    res
+      .status(400)
+      .json({ message: 'O body da requisição deve conter o campo name' })
+  }
+})
+
 export default effectTypeRoutes
